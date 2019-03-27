096061d056f7c82e57b0f7bb154f95c576915da1
huangxin <1377186691@qq.com>
2019-03-27 15:16:07 +0800

    remove unused import

--- src/main/java/com/netty/server/netty/handler/NettyWebSocketChannelInitializer.java
+++ src/main/java/com/netty/server/netty/handler/NettyWebSocketChannelInitializer.java
@@ -5,13 +5,9 @@ import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.http.HttpObjectAggregator;
-import io.netty.handler.codec.http.HttpRequestDecoder;
 import io.netty.handler.codec.http.HttpServerCodec;
 import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
 import io.netty.handler.stream.ChunkedWriteHandler;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
